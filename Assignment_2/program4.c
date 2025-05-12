fa78c8c51a26a8ff7ee2473e86fdd495b9325e41
prabhanjaningle <prabhanjan797@gmail.com>
2025-05-13 00:22:14 +0530

    Add files via upload

---
+++ Assignment_2/program4.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void Display( int iNo, int iFrequency)
+{
+    if(iFrequency < 0)
+    {
+        iFrequency = -iFrequency;
+    }
+    int iCnt = 0;
+    for(iCnt = 1; iCnt <= iFrequency; iCnt++)
+    {
+        printf(" %d ", iNo);
+    }
+}
+
+int main()
+{
+    int iValue = 0;
+    int iCount = 0;
+    printf("Enter the number : ");
+    scanf("%d" ,&iValue);
+    printf("Enter frequency : ");
+    scanf("%d" ,&iCount);
+    Display(iValue, iCount);
+    return 0;
+}
---
+++ Assignment_2/program5.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#define TRUE 1
+#define FALSE 0
+typedef int BOOL;
+BOOL ChkEven(int iNo)
+    {
+        if(iNo % 2 == 0)
+        {
+            return TRUE;
+        }
+        
+        else
+        {
+            return FALSE;
+        }
+    }
+int main()
+{
+    int iValue = 0;
+    BOOL bRet = FALSE;
+    printf("Enter number : ");
+    scanf("%d" ,&iValue);
+    bRet = ChkEven(iValue);
+    if(bRet == TRUE)
+    {
+        printf("It is even number");
+    }
+    else
+    {
+        printf("It is odd number");
+    }
+    return 0;
+}
