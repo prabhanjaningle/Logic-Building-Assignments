4417b80b438bbd17995a80a05f5aeefa1d115d01
prabhanjaningle <prabhanjan797@gmail.com>
2025-06-08 23:50:53 +0530

    Add files via upload

---
+++ Assignment_15/program5.c
@@ -0,0 +1,56 @@
+ ////////////////////////////////////////////////////////////////////////////////////////////
+//Author : Prabhanjan Sanjay Ingle
+//Description : Print the Pattern from given input rows and coloumns
+//Input : rows : 5 , Coloumns : 4
+// 1 2 3 4 5
+// 1 @ @ @ 5
+// 1 @ @ @ 5
+// 1 2 3 4 5
+//Date : 03-06-2025
+////////////////////////////////////////////////////////////////////////////////////////////
+#include <stdio.h>
+
+void Pattern(int iRow, int iCol)
+{
+    int iCnt1 = 0,iCnt2 = 0;
+   
+    for(iCnt1 = 1;iCnt1 <= iRow; iCnt1++)
+    {
+        
+        for(iCnt2 = 1;iCnt2 <= iCol;iCnt2++)
+        {
+            if((iCnt1 == 1 || iCnt1 == iRow || iCnt2 == 1 || iCnt2 == iCol))
+            {
+                printf("%d ",iCnt2);
+            }
+            else
+            {
+                printf("* ");
+            }
+            
+        }
+        
+        printf("\n");
+    }
+}
+
+
+int main()
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    printf("Enter rows  : \n");
+    scanf("%d", &iValue1);
+    printf("Enter coloumns : \n");
+    scanf("%d", &iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+
+
+
+}
+
+//Time complexity : O(N)
